ba4e31eff5f196828d451c71c3b87893efedf490
Eda Çam <54909611+edacaam@users.noreply.github.com>
2022-01-30 22:09:21 +0300

    Update reverse.js

--- Week6/part-3/reverse.js
+++ Week6/part-3/reverse.js
@@ -4,10 +4,10 @@ let reverse=(str)=> str.split("").reverse().join("");
 // string kopyalanır,daha sonra elemanların yeri ters çevirilir ve join ile birleştirilir.
 let reverse2=(str)=> [...str].reverse().join("");
 
-// stringin indexleri tersten okunarak boş bir stringe + ile ekelnir
+// stringin indexleri tersten okunarak boş bir stringe + ile eklenir
 let reverse3=(str)=>{
     let newString="";
-    for (var i = str.length - 1; i >= 0; i--) {
+    for (let i = str.length - 1; i >= 0; i--) {
         newString += str[i];
     }
     return newString;
@@ -17,7 +17,7 @@ let reverse3=(str)=>{
 // join metodu ile birleştirilir
 let reverse4=(str)=>{
     let newString=[];
-    for (var i = str.length - 1; i >= 0; i--) {
+    for (let i = str.length - 1; i >= 0; i--) {
         newString.push(str[i]);
     }
     return newString.join('');
